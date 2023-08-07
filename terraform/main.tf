34a3a021479adbbed70133886702c4686414a87c
robert altmiller <robert.altmiller@databricks.com>
2023-08-07 16:49:27 -0500

    uploading yml

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,11 +2,10 @@
 terraform {
   required_providers {
     aws = {
-      source = "hashicorp/aws"
-      Version = "~>5.0"
+      source  = "hashicorp/aws"
+      version = "~>5.0"
     }
   }
-  required_version = ">=0.14.9"
 }
 
 # configure the aws provider
