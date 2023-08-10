a9b726e9bb8e9bbc16ae9ffed6f1d798f0e1f18c
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 16:53:15 -0500

    changes

--- terraform/clusters/clusters.tf
+++ terraform/clusters/clusters.tf
@@ -14,12 +14,12 @@ resource "databricks_cluster" "this" {
   cluster_name            = "${local.cluster_config.cluster_name}-${var.environment}"
   node_type_id            = data.databricks_node_type.smallest.id
   spark_version           = data.databricks_spark_version.latest_lts.id
-  autotermination_minutes = format(%d, local.cluster_config.auto_termination_mins)
-  num_workers             = format(%d, local.cluster_config.min_workers)
+  autotermination_minutes = format("%d", local.cluster_config.auto_termination_mins)
+  num_workers             = format("%d", local.cluster_config.min_workers)
   
   autoscale {
-    min_workers = format(%d, local.cluster_config.min_workers)
-    max_workers = format(%d, local.cluster_config.max_workers)
+    min_workers = format("%d", local.cluster_config.min_workers)
+    max_workers = format("%d", local.cluster_config.max_workers)
   }
   
   custom_tags = {
