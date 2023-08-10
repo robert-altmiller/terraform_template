d449e6390cbdc2d0bc73974d642aaf975ecd1b83
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 10:12:57 -0500

    changes

--- providers.tf
+++ providers.tf
@@ -1,27 +1,14 @@
-# # required terraform providers
-# terraform {
-#   required_providers {
-#     aws = "~> 3.0"
-#   }
-# }
-
-# # configure the aws provider
-# provider "aws" {
-#   region     = var.AWS_REGION
-#   access_key = var.AWS_ACCESS_KEY_ID
-#   secret_key =  var.AWS_SECRET_ACCESS_KEY
-# }
-
 # required terraform providers
 terraform {
   required_providers {
-    aws = "~> 5.0"
+    aws = "~> 3.0"
   }
+  required_version = ">=0.14.9"
 }
 
 # configure the aws provider
 provider "aws" {
-  region     = local.aws_region_output
-  access_key = local.aws_access_key_id_output
-  secret_key =  local.aws_secret_access_key_output
+  region     = var.aws_region
+  access_key = var.aws_access_key_id
+  secret_key =  var.aws_secret_access_key
 }
