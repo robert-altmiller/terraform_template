e5b9f5bae86e49f88e96cf107f00c5e425c13129
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 14:34:35 -0500

    changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -23,6 +23,6 @@ provider "aws" {
 provider "databricks" {
   alias    = "mws"
   host     = "https://accounts.cloud.databricks.com"
-  client_id = var.databricks_client_id
-  client_secret = var.databricks_client_secret
+  client_id = local.databricks_client_id
+  client_secret = local.databricks_client_secret
 }
