e0147acef080e098d0b52c0e45f03448869e264f
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 17:22:56 -0500

    changes

---
+++ terraform/clusters/providers.tf
@@ -0,0 +1,10 @@
+// Initialize provider in "MWS" mode to provision the new workspace.
+// alias = "mws" instructs Databricks to connect to https://accounts.cloud.databricks.com, to create
+// a Databricks workspace that uses the E2 version of the Databricks on AWS platform.
+// See https://registry.terraform.io/providers/databricks/databricks/latest/docs#authentication
+provider "databricks" {
+  alias    = "mws"
+  host     = "https://accounts.cloud.databricks.com"
+  client_id = local.databricks_client_id
+  client_secret = local.databricks_client_secret
+}
