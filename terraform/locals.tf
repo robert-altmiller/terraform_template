ba2c80b4a902fdbfebd242710c7ce2b9a4ed10a9
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 14:52:07 -0500

    changes

---
+++ terraform/locals.tf
@@ -0,0 +1,44 @@
+# source directory
+locals {
+  source_dir = "${var.source_dir}"
+}
+
+# aws region
+locals {
+  aws_region = "${var.aws_region}"
+}
+
+# aws access key id
+locals {
+  aws_access_key_id = "${var.aws_access_key_id}"
+}
+
+# aws access key secret
+locals {
+  aws_access_key_secret = "${var.aws_access_key_secret}"
+}
+
+# dbricks instance url
+locals {
+  databricks_instance = "${var.databricks_instance}"
+}
+
+# dbricks admin login
+locals {
+  databricks_admin_login = "${var.databricks_admin_login}"
+}
+
+# dbricks admin password
+locals {
+  databricks_admin_password = "${var.databricks_admin_password}"
+}
+
+# client id
+locals {
+  databricks_client_id = lookup("${var.databricks_client_ids}", "${var.environment}")
+}
+
+# client secret
+locals {
+  databricks_client_secret = lookup("${var.databricks_client_secrets}", "${var.environment}")
+}
