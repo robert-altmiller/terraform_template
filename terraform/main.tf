7e86d4ffe52bc7a7278a3c692fd4128fc3cee824
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 13:11:17 -0500

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,16 +3,16 @@ locals {
 }
 
 locals {
-  aws_region_redacted = "${var.aws_region}"
+  aws_region = "${var.aws_region}"
 }
 
 locals {
-  aws_access_key_id_redacted = "${var.aws_access_key_id}"
+  aws_access_key_id = "${var.aws_access_key_id}"
 }
 
 
 locals {
-  aws_access_key_secret_redacted = "${var.aws_access_key_secret}"
+  aws_access_key_secret = "${var.aws_access_key_secret}"
 }
 
 output "source_dir_output" {
@@ -21,16 +21,16 @@ output "source_dir_output" {
 }
 
 output "aws_region_output" {
-  value = nonsensitive(local.aws_region_redacted)
+  value = local.aws_region
   description = "redacted version of aws_region"
 }
 
 output "aws_access_key_id_output" {
-  value = nonsensitive(local.aws_access_key_id_redacted)
+  value = nonsensitive(local.aws_access_key_id)
   description = "redacted version of aws_access_key_id"
 }
 
 output "aws_access_key_secret_output" {
-  value = local.aws_access_key_secret_redacted
+  value = nonsensitive(local.aws_access_key_secret)
   description = "redacted version of aws_access_key_secret"
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -9,16 +9,19 @@ variable "source_dir" {
 variable "aws_region" {
   description = "AWS_REGION secret passed from GitHub Actions"
   type        = string
+  sensitive   = false
 }
 
 # aws access key id environment variable
 variable "aws_access_key_id"{
   description = "AWS_ACCESS_KEY secret passed from GitHub Actions"
   type        = string
+  sensitive   = true
 }
 
 # aws secret access key  environment variable
 variable "aws_access_key_secret" {
   description = "AWS_SECRET_ACCESS_KEY secret passed from GitHub Actions"
   type        = string
+  sensitive   = true
 }
