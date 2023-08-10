55a2b0ba0ac72aa7f20a53b8a68fa2fc8f4b3d22
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 13:23:01 -0500

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -25,29 +25,35 @@ locals {
 output "source_dir_output" {
   value = jsonencode(local.source_dir)
   description = "value for source_dir"
+  sensitive = true
 }
 
 output "aws_region_output" {
   value = jsonencode(local.aws_region)
   description = "value for aws_region"
+  sensitive = true
 }
 
 output "aws_access_key_id_output" {
   value = jsonencode(local.aws_access_key_id)
   description = "value for aws_access_key_id"
+  sensitive = true
 }
 
 output "aws_access_key_secret_output" {
   value = jsonencode(local.aws_access_key_secret)
   description = "value for aws_access_key_secret"
+  sensitive = true
 }
 
 output "databricks_admin_login_output" {
   value = jsonencode(local.databricks_admin_login)
   description = "value for databricks_admin_login"
+  sensitive = true
 }
 
 output "databricks_admin_password_output" {
   value = jsonencode(local.databricks_admin_password)
   description = "value for databricks_admin_password"
+  sensitive = true
 }
