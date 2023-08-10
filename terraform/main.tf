1fa72923474b11b694da51b0501e6b0eddda9855
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 14:25:59 -0500

    changes

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,48 +1,77 @@
+# source directory
 locals {
   source_dir = "${var.source_dir}"
 }
 
+# aws region
 locals {
   aws_region = "${var.aws_region}"
 }
 
+# aws access key id
 locals {
   aws_access_key_id = "${var.aws_access_key_id}"
 }
 
+# aws access key secret
 locals {
   aws_access_key_secret = "${var.aws_access_key_secret}"
 }
 
+# dbricks instance url
+locals {
+  databricks_instance = "${var.databricks_instance}"
+}
+
+# dbricks admin login
 locals {
   databricks_admin_login = "${var.databricks_admin_login}"
 }
 
+# dbricks admin password
 locals {
   databricks_admin_password = "${var.databricks_admin_password}"
 }
 
+# client id
+locals {
+  databricks_client_id = lookup(var.databricks_client_ids, var.environment)
+}
+
+# client secret
+locals {
+  databricks_client_secret = lookup(var.databricks_client_secrets, var.environment)
+}
+
+
+
 output "source_dir_output" {
   value = jsonencode(local.source_dir)
   description = "value for source_dir"
-  sensitive = false
+  sensitive = true
 }
 
 output "aws_region_output" {
   value = jsonencode(local.aws_region)
   description = "value for aws_region"
-  sensitive = false
+  sensitive = true
 }
 
 output "aws_access_key_id_output" {
   value = jsonencode(local.aws_access_key_id)
   description = "value for aws_access_key_id"
-  sensitive = false
+  sensitive = true
 }
 
 output "aws_access_key_secret_output" {
   value = jsonencode(local.aws_access_key_secret)
   description = "value for aws_access_key_secret"
+  sensitive = true
+}
+
+output "databricks_instance_output" {
+  value = jsonencode(local.databricks_instance)
+  description = "value for databricks_instance"
   sensitive = true
 }
 
@@ -54,6 +83,18 @@ output "databricks_admin_login_output" {
 
 output "databricks_admin_password_output" {
   value = jsonencode(local.databricks_admin_password)
+  description = "value for databricks_admin_password"
+  sensitive = true
+}
+
+output "databricks_client_id_output" {
+  value = jsonencode(local.databricks_client_id)
+  description = "value for databricks_admin_password"
+  sensitive = true
+}
+
+output "databricks_client_secret_output" {
+  value = jsonencode(local.databricks_client_secret)
   description = "value for databricks_admin_password"
   sensitive = true
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,42 +1,59 @@
 # aws region environment variable
+variable "environment" {
+  description = "deployment environment"
+  type        = string
+}
+
 variable "source_dir" {
   description = "local github repo source directory"
   type        = string
   default     = "terraform"
-  sensitive   = true
 }
 
 # aws region environment variable
 variable "aws_region" {
   description = "AWS_REGION secret passed from GitHub Actions"
   type        = string
-  sensitive   = true
 }
 
 # aws access key id environment variable
 variable "aws_access_key_id"{
   description = "AWS_ACCESS_KEY secret passed from GitHub Actions"
   type        = string
-  sensitive   = true
 }
 
-# aws secret access key  environment variable
+# aws secret access key environment variable
 variable "aws_access_key_secret" {
   description = "AWS_SECRET_ACCESS_KEY secret passed from GitHub Actions"
   type        = string
-  sensitive   = true
 }
 
-# aws secret access key  environment variable
+# databricks instance environment variable
+variable "databricks_instance" {
+  description = "DATABRICKS_INSTANCE secret passed from GitHub Actions"
+  type        = string
+}
+
+# databricks admin login environment variable
 variable "databricks_admin_login" {
   description = "DATABRICKS_ADMIN_LOGIN secret passed from GitHub Actions"
   type        = string
-  sensitive   = true
 }
 
-# aws secret access key  environment variable
+# databricks admin password environment variable
 variable "databricks_admin_password" {
   description = "DATABRICKS_ADMIN_PASSWORD secret passed from GitHub Actions"
   type        = string
-  sensitive   = true
+}
+
+# databricks client id environment variable (e.g. service principle id)
+variable "databricks_client_ids" {
+  description = "Map of Databricks client ids for different environments"
+  type = map(string)
+}
+
+# databricks client secret environment variable (e.g. service principle secret)
+variable "databricks_client_secrets" {
+  description = "Map of Databricks client secrets for different environments"
+  type = map(string)
 }
