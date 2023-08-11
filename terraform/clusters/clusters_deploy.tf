44b05d75efd1e31c50b4dbdc14f7b70f06847047
robert altmiller <robert.altmiller@databricks.com>
2023-08-10 23:03:01 -0500

    changes

--- terraform/clusters/clusters_deploy.tf
+++ terraform/clusters/clusters_deploy.tf
@@ -9,13 +9,14 @@ data "databricks_node_type" "smallest" {
 data "databricks_spark_version" "latest_lts" {
   provider = databricks.workspace
   ml = true
+  latest = true
 }
 
 resource "databricks_cluster" "this" {
   provider                = databricks.workspace
   cluster_name            = "${local.cluster_config.cluster_name}-${var.environment}"
   node_type_id            = data.databricks_node_type.smallest.id
-  spark_version           =  data.databricks_spark_version.latest_lts.id #"12.2.x-scala2.12"
+  spark_version           =  data.databricks_spark_version.latest_lts.id
   autotermination_minutes = format("%d", local.cluster_config.auto_termination_mins)
   num_workers             = format("%d", local.cluster_config.min_workers)
   
