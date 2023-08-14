a1f3a08215dc84cd7cc3f30278e0ed580d518611
robert altmiller <robert.altmiller@databricks.com>
2023-08-14 16:51:20 -0500

    changes

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -34,6 +34,7 @@ provider "databricks" {
 provider "databricks" {
   alias = "workspace"
   host = local.databricks_instance
+  account_id = local.databricks_account_id
   client_id = local.databricks_client_id
   client_secret = local.databricks_client_secret
 }
