f42304dd7dae7711c7922127dc4e3aac578bcdd7
robert altmiller <robert.altmiller@databricks.com>
2023-08-14 14:54:41 -0500

    changes

--- terraform/modules.tf
+++ terraform/modules.tf
@@ -4,9 +4,6 @@ variable "execute_cluster_module" {
   default     = false
 }
 
-resource "cluster_module_resource" {
-  count = var.execute_cluster_module ? 1 : 0
-}
 
 variable "execute_uc_sc_module" {
   description = "Set to true to execute the unity catalog  module, false to skip."
@@ -14,13 +11,10 @@ variable "execute_uc_sc_module" {
   default     = false
 }
 
-resource "uc_sc_module_resource" {
-  count = var.execute_uc_sc_module ? 1 : 0
-}
-
 # initialize cluster module with root level provider settings (inherited)
 module "cluster_module" {
   source = "./clusters"
+  execute_cluster_module = var.execute_cluster_module
   github_actor = var.github_actor
   environment = var.environment 
   databricks_account_id = local.databricks_account_id
@@ -35,6 +29,7 @@ module "cluster_module" {
 # initialize unity catalog storage credential (sc) with root level provider settings (inherited)
 module "uc_sc_module" {
   source = "./unity_catalog/storage_creds"
+  execute_uc_sc_module = var.execute_uc_sc_module
   github_actor = var.github_actor
   environment = var.environment 
   databricks_account_id = local.databricks_account_id
