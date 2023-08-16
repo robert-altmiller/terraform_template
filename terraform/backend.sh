eb188cb72da374c87b94a36ef686c5005affc251
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 09:46:18 -0500

    changes

---
+++ terraform/backend.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set your variables here
+# BUCKET_NAME="dbricks-dev-bucket"
+# KEY_NAME="terraform/terraform.tfstate"
+# REGION="us-west-2"
+# PROFILE="awscredsprofile"
+
+# Run terraform init with the arguments
+terraform init \
+  -backend-config="bucket=$BUCKET_NAME" \
+  -backend-config="key=$KEY_NAME" \
+  -backend-config="region=$AWS_REGION" \
+  -backend-config="access_key=$ACCESS_KEY" \
+  -backend-config="secret-key=$SECRET_KEY" \
+  # -backend-config="shared_credentials_file=./.aws/credentials" \
+  # -backend-config="profile=$PROFILE"
