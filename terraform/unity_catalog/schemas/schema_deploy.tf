bc30e5c495f1c52dc193543336d17381c5ba29e4
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 16:34:37 -0500

    changes

--- terraform/unity_catalog/schemas/schema_deploy.tf
+++ terraform/unity_catalog/schemas/schema_deploy.tf
@@ -8,6 +8,7 @@ resource "databricks_schema" "schema" {
 
 # databricks_grants on schema (UC)
 resource "databricks_grants" "schema" {
+  provider = databricks.workspace
   schema = "${local.schema_config.catalog_name}.${local.schema_config.schema_name }"
   grant {
     principal  = local.schema_config.principal_name
--- terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
+++ terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
@@ -10,7 +10,7 @@ resource "databricks_storage_credential" "external" {
 
 # databricks_grants on storage credential (UC)
 resource "databricks_grants" "external_creds" {
-  #depends_on = [databricks_storage_credential.external]
+  provider = databricks.workspace
   count = var.databricks_deploy_uc_storage_credential == "true" ? 1 : 0 # used as a conditional
   provider = databricks.workspace  
   storage_credential = databricks_storage_credential.external.id
