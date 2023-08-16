0367f8201a2ee56774dfec1b7e60f02a3658720a
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 11:58:40 -0500

    changes

--- terraform/backend.sh
+++ terraform/backend.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
-# Set your variables here
-# BUCKET_NAME="dbricks-dev-bucket"
-# KEY_NAME="terraform/terraform.tfstate"
-# REGION="us-west-2"
-# PROFILE="awscredsprofile"
-
 # Run terraform init with the arguments
+# Terraform backend configuration
 terraform init \
   -backend-config="bucket=$BUCKET_NAME" \
   -backend-config="key=$KEY_NAME" \
   -backend-config="region=$AWS_REGION" \
   -backend-config="access_key=$ACCESS_KEY" \
-  -backend-config="secret_key=$SECRET_KEY" \
-  # -backend-config="shared_credentials_file=./.aws/credentials" \
-  # -backend-config="profile=$PROFILE"
+  -backend-config="secret_key=$SECRET_KEY"
--- terraform/clusters/clusters_deploy.tf
+++ terraform/clusters/clusters_deploy.tf
@@ -13,7 +13,7 @@ data "databricks_spark_version" "latest_lts" {
 }
 
 resource "databricks_cluster" "this" {
-  count                   = var.databricks_deploy_clusters == "true" ? 1 : 0
+  count                   = var.databricks_deploy_clusters == "true" ? 1 : 0 # used as a conditional
   provider                = databricks.workspace
   cluster_name            = "${local.cluster_config.cluster_name}-${var.environment}"
   node_type_id            = data.databricks_node_type.smallest.id
