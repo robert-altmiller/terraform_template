f4b0b766284d4daebd1bcad481722dd51d7b37af
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 22:41:53 -0500

    changes

--- terraform/summarize_plan.sh
+++ terraform/summarize_plan.sh
@@ -4,23 +4,23 @@
 terraform plan -detailed-exitcode > plan_output.txt 2>&1
 
 # Check the exit code
-exit_code=${PIPESTATUS[0]}
-case $exit_code in
-  0)
-    echo "No changes."
-    ;;
-  1)
-    echo "Error running terraform plan."
-    ;;
-  2)
-    echo "Changes to be made:"
-    # Use grep and awk to extract and summarize the changes
-    cat plan_output.txt | grep -E '\s+[~+-]' | awk '{print $1 " " $2}' | sort | uniq -c
-    ;;
-  *)
-    echo "Unknown exit code."
-    ;;
-esac
+# exit_code=${PIPESTATUS[0]}
+# case $exit_code in
+#   0)
+#     echo "No changes."
+#     ;;
+#   1)
+#     echo "Error running terraform plan."
+#     ;;
+#   2)
+#     echo "Changes to be made:"
+#     # Use grep and awk to extract and summarize the changes
+#     cat plan_output.txt | grep -E '\s+[~+-]' | awk '{print $1 " " $2}' | sort | uniq -c
+#     ;;
+#   *)
+#     echo "Unknown exit code."
+#     ;;
+# esac
 
-# Clean up the temporary file
-rm plan_output.txt
+# # Clean up the temporary file
+# rm plan_output.txt
