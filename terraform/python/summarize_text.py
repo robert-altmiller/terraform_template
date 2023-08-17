27d04ca5e04700ce34f339d766718527719281b0
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 23:36:12 -0500

    changes

--- terraform/python/summarize_text.py
+++ terraform/python/summarize_text.py
@@ -1,15 +1,17 @@
-from transformers import BartForConditionalGeneration, BartTokenizer
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
 import sys
 
 def summarize_text(text):
-    model_name = "facebook/bart-large-cnn"
-    model = BartForConditionalGeneration.from_pretrained(model_name)
-    tokenizer = BartTokenizer.from_pretrained(model_name)
-    
-    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=1024, truncation=True)
-    summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    model_name = "amazon/FalconLite"
+    model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+    tokenizer = AutoTokenizer.from_pretrained(model_name)
+
+    # Encode the text and generate the summary
+    inputs = tokenizer([text], max_length=1024, return_tensors="pt", truncation=True)
+    summary_ids = model.generate(inputs.input_ids, num_beams=4, length_penalty=2.0, max_length=142, min_length=30, no_repeat_ngram_size=3)
+
+    # Decode and return the summary
     summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
-    
     return summary
 
 if __name__ == '__main__':
