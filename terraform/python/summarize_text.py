b3df9dc83fb0b8c0d7cba6630d0367d9c955e85d
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 23:23:53 -0500

    changes

---
+++ terraform/python/summarize_text.py
@@ -0,0 +1,17 @@
+from transformers import BartForConditionalGeneration, BartTokenizer
+import sys
+
+def summarize_text(text):
+    model_name = "facebook/bart-large-cnn"
+    model = BartForConditionalGeneration.from_pretrained(model_name)
+    tokenizer = BartTokenizer.from_pretrained(model_name)
+    
+    inputs = tokenizer.encode("summarize: " + text, return_tensors="pt", max_length=1024, truncation=True)
+    summary_ids = model.generate(inputs, max_length=150, min_length=40, length_penalty=2.0, num_beams=4, early_stopping=True)
+    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    
+    return summary
+
+if __name__ == '__main__':
+    text = sys.stdin.read()
+    print(summarize_text(text))
--- terraform/tf_plan_summary.sh
+++ terraform/tf_plan_summary.sh
@@ -1,30 +1,4 @@
 #!/bin/bash
 
 # Run terraform plan and save the output to a file
-terraform plan > plan_output.txt
-
-# Initialize an empty array to hold the summary lines
-declare -a summary_lines
-
-# Read the plan output line by line and extract the relevant information
-while IFS= read -r line; do
-  if [[ $line == "# "* ]]; then
-    resource_type=$(echo "$line" | awk '{print $2}' | cut -d'.' -f2)
-    resource_name=$(echo "$line" | awk '{print $2}' | cut -d'.' -f3)
-    operation=$(echo "$line" | awk '{print $3}' | awk -F'[' '{print $1}' | tr -d ' ')
-    summary=$(echo "$line" | cut -d'#' -f2)
-    id_line=$(grep -A1 "id =" plan_output.txt | tail -n1)
-    resource_id=$(echo "$id_line" | awk -F'= ' '{print $2}' | tr -d ' ' | tr -d '"')
-    summary_line="$operation $resource_type $resource_name $resource_id \"$summary\""
-    summary_lines+=("$summary_line")
-  fi
-done < plan_output.txt
-
-# Output the table with headers
-echo -e "Operation\tResource Type\tResource Name\tResource ID\tSummary"
-for line in "${summary_lines[@]}"; do
-  echo -e "$line"
-done
-
-# Remove the temporary plan output file
-rm plan_output.txt
+terraform plan | python3 ./python/summarize_text.py
