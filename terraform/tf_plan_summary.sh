91cbea60adb61263e8a15d445b6c097eff440c50
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 23:12:33 -0500

    changes

--- terraform/tf_plan_summary.sh
+++ terraform/tf_plan_summary.sh
@@ -2,14 +2,21 @@
 
 plan_output=$(terraform plan)
 
-# Extract the operations, resource types, and resource names
-actions=$(echo "$plan_output" | grep -E '\s+# [^ ]+ will be' | awk -F' ' '{print $4}')
-resources=$(echo "$plan_output" | grep -E '\s+# [^ ]+ will be' | awk -F'# ' '{print $2}' | awk '{print $1}')
+# Filter out the relevant section of the terraform plan output
+relevant_section=$(echo "$plan_output" | awk '/Terraform will perform the following actions:/,/Plan:/{print}')
+
+# Extract the operations, resource types, resource names, and resource ids
+resource_entries=$(echo "$relevant_section" | grep -E '\s+# [^ ]+ will be')
+operations=$(echo "$resource_entries" | awk -F' ' '{print $4}')
+resource_ids=$(echo "$relevant_section" | grep -E '^\s+- id[ \t]*=' | awk -F'= ' '{print $2}' | tr -d '"' | tr -d '-> null')
 
 # Extract resource types and resource names
-resource_types=$(echo "$resources" | awk -F'.' '{print $2}')
-resource_names=$(echo "$resources" | awk -F'.' '{print substr($0, index($0,$4))}')
+resource_types=$(echo "$resource_entries" | awk -F'# ' '{print $2}' | awk -F'.' '{print $2}')
+resource_names=$(echo "$resource_entries" | awk -F'# ' '{print $2}' | awk '{print substr($0, index($0,$3))}')
+
+# Extract a short summary of what's happening (using the first hashtag)
+summaries=$(echo "$relevant_section" | awk -F'# ' 'NF>1{print $2}' | awk -F' will be ' '{print $1}')
 
 # Display the summarized output in a table
-echo -e "Operation\tResource Type\tResource Name"
-paste <(echo "$actions") <(echo "$resource_types") <(echo "$resource_names") | column -t
+echo -e "Operation\tResource Type\tResource Name\tResource ID\tSummary"
+paste <(echo "$operations") <(echo "$resource_types") <(echo "$resource_names") <(echo "$resource_ids") <(echo "$summaries") | column -t -s $'\t'
