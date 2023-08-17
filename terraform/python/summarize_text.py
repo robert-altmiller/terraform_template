31f0af9a42f5cd2d86d51e9f7ab6bf09df8ae437
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 23:56:51 -0500

    changes

--- terraform/python/summarize_text.py
+++ terraform/python/summarize_text.py
@@ -1,17 +1,13 @@
-from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import PegasusForConditionalGeneration, PegasusTokenizer
 import sys
 
 def summarize_text(text):
     model_name = "amazon/FalconLite"
-    model = AutoModelForSeq2SeqLM.from_pretrained(model_name, trust_remote_code=True)
-    tokenizer = AutoTokenizer.from_pretrained(model_name)
-
-    # Encode the text and generate the summary
-    inputs = tokenizer([text], max_length=1024, return_tensors="pt", truncation=True)
-    summary_ids = model.generate(inputs.input_ids, num_beams=4, length_penalty=2.0, max_length=142, min_length=30, no_repeat_ngram_size=3)
-
-    # Decode and return the summary
-    summary = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    tokenizer = PegasusTokenizer.from_pretrained(model_name)
+    model = PegasusForConditionalGeneration.from_pretrained(model_name, trust_remote_code=True)
+    inputs = tokenizer([text], max_length=512, return_tensors="pt", truncation=True)
+    outputs = model.generate(inputs.input_ids)
+    summary = tokenizer.decode(outputs[0], skip_special_tokens=True)
     return summary
 
 if __name__ == '__main__':
