347ad8a2b46ff6120dfb985cf91dd3e6cc83cbab
robert altmiller <robert.altmiller@databricks.com>
2023-08-17 18:05:16 -0500

    changes

--- terraform/workspace/cluster_policies/cluster_policies_config.tf
+++ terraform/workspace/cluster_policies/cluster_policies_config.tf
@@ -20,51 +20,51 @@ variable "cluster_policy_config_json" {
     cluster_policy_config = jsondecode(var.cluster_policy_config_json)
   }
 
-  locals {
-      cluster_policy_details = <<-EOT
-      {
-        "node_type_id": {
-          "type": "allowlist",
-          "values": local.cluster_policy_config.node_type_id,
-          "defaultValue": "i3.xlarge"
-        },
-        "spark_version": {
-          "type": "unlimited",
-          "defaultValue": local.cluster_policy_config.spark_version,
-        },
-        "runtime_engine": {
-          "type": "fixed",
-          "value": local.cluster_policy_config.spark_version,
-          "hidden": true
-        },
-        "num_workers": {
-          "type": "fixed",
-          "value": local.cluster_policy_config.spark_version,
-          "hidden": true
-        },
-        "driver_instance_pool_id": {
-          "type": "forbidden",
-          "hidden": true
-        },
-        "cluster_type": {
-          "type": "fixed",
-          "value": local.cluster_policy_config.cluster_type,
-        },
-        "instance_pool_id": {
-          "type": "forbidden",
-          "hidden": true
-        },
-        "spark_conf.spark.databricks.cluster.profile": {
-          "type": "fixed",
-          "value": "singleNode",
-          "hidden": true
-        },
-        "autotermination_minutes": {
-          "type": "unlimited",
-          "defaultValue": local.cluster_policy_config.auto_termination_mins,
-          "isOptional": true
-        }
+locals {
+  cluster_policy_details = <<-EOT
+    {
+      "node_type_id": {
+        "type": "allowlist",
+        "values": local.cluster_policy_config.node_type_id,
+        "defaultValue": "i3.xlarge"
+      },
+      "spark_version": {
+        "type": "unlimited",
+        "defaultValue": local.cluster_policy_config.spark_version
+      },
+      "runtime_engine": {
+        "type": "fixed",
+        "value": local.cluster_policy_config.spark_version,
+        "hidden": true
+      },
+      "num_workers": {
+        "type": "fixed",
+        "value": local.cluster_policy_config.spark_version,
+        "hidden": true
+      },
+      "driver_instance_pool_id": {
+        "type": "forbidden",
+        "hidden": true
+      },
+      "cluster_type": {
+        "type": "fixed",
+        "value": local.cluster_policy_config.cluster_type
+      },
+      "instance_pool_id": {
+        "type": "forbidden",
+        "hidden": true
+      },
+      "spark_conf.spark.databricks.cluster.profile": {
+        "type": "fixed",
+        "value": "singleNode",
+        "hidden": true
+      },
+      "autotermination_minutes": {
+        "type": "unlimited",
+        "defaultValue": local.cluster_policy_config.auto_termination_mins,
+        "isOptional": true
       }
-    EOT
-  }
+    }
+  EOT
+}
 
