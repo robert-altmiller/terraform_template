b66066d0faaf292173a6bdbb71a73de12356cea9
robert altmiller <robert.altmiller@databricks.com>
2023-08-17 18:07:30 -0500

    changes

--- terraform/workspace/cluster_policies/clusters_policies_deploy.tf
+++ terraform/workspace/cluster_policies/clusters_policies_deploy.tf
@@ -2,5 +2,5 @@ resource "databricks_cluster_policy" "policy" {
   provider = databricks.workspace
   count = var.databricks_deploy_cluster_policies == "true" ? 1 : 0 # used as a conditional
   name       = "${local.cluster_policy_config.cluster_policy_name}-${var.environment}"
-  definition = local.cluster_policy_details
+  definition = jsonencode(local.cluster_policy_details)
 }
