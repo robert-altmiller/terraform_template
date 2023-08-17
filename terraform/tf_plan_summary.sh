d282b42af6f3201c6a012db2d63e25be17ced774
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 22:57:17 -0500

    changes

--- terraform/tf_plan_summary.sh
+++ terraform/tf_plan_summary.sh
@@ -3,8 +3,8 @@
 plan_output=$(terraform plan)
 
 # Extract the actions and resources
-actions=$(echo "$plan_output" | grep -E '# .+ will be' | awk -F'# ' '{print $2}' | awk -F' ' '{print $1}' | sed 's/module.\+\.//')
-resources=$(echo "$plan_output" | grep -E '# .+ will be' | awk -F'# ' '{print $2}' | awk -F' ' '{print $2}' | sed 's/module.\+\.//')
+actions=$(echo "$plan_output" | grep -E '\s+# [^ ]+ will be' | awk -F' ' '{print $3}')
+resources=$(echo "$plan_output" | grep -E '\s+# [^ ]+ will be' | awk -F'# ' '{print $2}' | awk '{print $1}')
 
 # Display the summarized output in a table
 echo -e "Operation\tResource Type\tResource Name"
