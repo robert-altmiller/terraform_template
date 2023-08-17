68dc358f0eeac152a8d3a80641c137fb31589fde
robert altmiller <robert.altmiller@databricks.com>
2023-08-16 22:39:38 -0500

    changes

--- terraform/summarize_plan.sh
+++ terraform/summarize_plan.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Run terraform plan and save the output
-plan_output=$(terraform plan -detailed-exitcode)
+# Run terraform plan and save the output to a file
+terraform plan -detailed-exitcode > plan_output.txt 2>&1
 
 # Check the exit code
-case $? in
+exit_code=${PIPESTATUS[0]}
+case $exit_code in
   0)
     echo "No changes."
     ;;
@@ -14,9 +15,12 @@ case $? in
   2)
     echo "Changes to be made:"
     # Use grep and awk to extract and summarize the changes
-    echo "$plan_output" | grep -E '\s+[~+-]' | awk '{print $1 " " $2}' | sort | uniq -c
+    cat plan_output.txt | grep -E '\s+[~+-]' | awk '{print $1 " " $2}' | sort | uniq -c
     ;;
   *)
     echo "Unknown exit code."
     ;;
 esac
+
+# Clean up the temporary file
+rm plan_output.txt
