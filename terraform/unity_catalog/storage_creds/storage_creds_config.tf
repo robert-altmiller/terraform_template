fe00488e4e1ba1d0e35a0f2501a192e21864e547
robert altmiller <robert.altmiller@databricks.com>
2023-08-18 11:51:13 -0500

    changes

--- terraform/unity_catalog/storage_creds/storage_creds_config.tf
+++ terraform/unity_catalog/storage_creds/storage_creds_config.tf
@@ -6,7 +6,7 @@ variable "sc_config_json" {
   {
     "sc_name": "my-storage-creds7",
     "sc_role_arn": "arn:aws:iam::969341422355:role/s3-dbricks-prod-bucket-role",
-    "principal_name": "prod-contributors",
+    "principal_name": "dev-contributors",
     "principal_privileges": ["ALL_PRIVILEGES"]
   }
   EOT
