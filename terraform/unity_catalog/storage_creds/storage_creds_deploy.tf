d863983b4949a85c844a1ad0706ebd2eadd6a9dc
robert altmiller <robert.altmiller@databricks.com>
2023-08-22 16:43:22 -0500

    changes

--- terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
+++ terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
@@ -1,21 +1,21 @@
 # create databricks storage credential (UC)
 resource "databricks_storage_credential" "external" {
-  provider = databricks.workspace
-  name = "${local.sc_config.sc_name}-${var.environment}"
+  for_each = local.storage_credentials
+  provider  = databricks.workspace
+  name      = "${each.value.sc_name}-${var.environment}"
   aws_iam_role {
-    role_arn = local.sc_config.sc_role_arn
+    role_arn = each.value.sc_role_arn
   }
   comment = "Managed by Terraform (TF)"
 }
 
-# add databricks grants on storage credential (UC)
 resource "databricks_grants" "credential_grants" {
-  depends_on = [databricks_storage_credential.external]
-  #count = var.databricks_deploy_uc_storage_credential == "true" ? 1 : 0 # used as a conditional because of 'databricks_storage_credential.external.id'
-  provider = databricks.workspace
-  storage_credential = databricks_storage_credential.external.id
+  count = var.databricks_deploy_uc_storage_credential == "true" ? 1 : 0 # used as a conditional because of 'databricks_storage_credential.external.id'
+  for_each = local.storage_credentials
+  provider           = databricks.workspace
+  storage_credential = databricks_storage_credential.external[each.key].id
   grant {
-    principal  = "${var.environment}-${local.sc_config.sc_principal_name}"
-    privileges = local.sc_config.sc_principal_privileges
+    principal  = "${var.environment}-${each.value.sc_principal_name}"
+    privileges = each.value.sc_principal_privileges
   }
 }
