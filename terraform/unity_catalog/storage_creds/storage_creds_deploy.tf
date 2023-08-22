33b14b31b1b5c8ca55bfecb6f115393dcaf8bbeb
robert altmiller <robert.altmiller@databricks.com>
2023-08-22 16:46:03 -0500

    changes

--- terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
+++ terraform/unity_catalog/storage_creds/storage_creds_deploy.tf
@@ -1,6 +1,6 @@
 # create databricks storage credential (UC)
 resource "databricks_storage_credential" "external" {
-  for_each = local.storage_credentials
+  for_each = local.sc_config.storage_credentials
   provider  = databricks.workspace
   name      = "${each.value.sc_name}-${var.environment}"
   aws_iam_role {
@@ -11,7 +11,7 @@ resource "databricks_storage_credential" "external" {
 
 resource "databricks_grants" "credential_grants" {
   # count = var.databricks_deploy_uc_storage_credential == "true" ? 1 : 0 # used as a conditional because of 'databricks_storage_credential.external.id'
-  for_each = local.storage_credentials
+  for_each = local.sc_config.storage_credentials
   provider           = databricks.workspace
   storage_credential = databricks_storage_credential.external[each.key].id
   grant {
