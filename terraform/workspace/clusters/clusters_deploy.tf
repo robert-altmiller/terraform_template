40fd9e8c769d2c8bb0ef1531c2a866bb451fadcb
robert altmiller <robert.altmiller@databricks.com>
2023-09-20 17:17:46 -0500

    changes

--- terraform/workspace/clusters/clusters_deploy.tf
+++ terraform/workspace/clusters/clusters_deploy.tf
@@ -1,36 +1,3 @@
-# # create the cluster with the "smallest" amount of resources allowed
-# data "databricks_node_type" "smallest" {
-#   local_disk = true
-# }
-
-# # use the latest Databricks Runtime
-# data "databricks_spark_version" "latest_lts" {
-#   provider = databricks.workspace
-#   ml = true
-#   latest = true
-# }
-
-# # create databrickls cluster
-# resource "databricks_cluster" "create_cluster" {
-#   for_each                 = jsondecode(var.databricks_submission_json)["clusters"]
-#   provider                 = databricks.workspace
-#   cluster_name             = tostring(lookup(each.value, "resource_name", ""))
-#   node_type_id             = data.databricks_node_type.smallest.id
-#   spark_version            = data.databricks_spark_version.latest_lts.id
-#   autotermination_minutes  = tonumber(lookup(each.value, "auto_termination_mins", "30"))
-#   num_workers              = tonumber(lookup(each.value, "min_workers", "1"))
-  
-#   autoscale {
-#     min_workers = tonumber(lookup(each.value, "min_workers", "1"))
-#     max_workers = tonumber(lookup(each.value, "max_workers", "2"))
-#   }
-
-#   custom_tags = {
-#     environment = var.environment
-#     owner       = var.github_actor
-#   }
-# }
-
 # create the cluster with the "smallest" amount of resources allowed
 data "databricks_node_type" "smallest" {
   local_disk = true
